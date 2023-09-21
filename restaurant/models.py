c06aef5313caab387db314fa8e6394eab9d77e8c
albertnguyen97 <chaunm.hmc@gmail.com>
2023-09-21 23:35:25 +0700

    fix booking

--- restaurant/models.py
+++ restaurant/models.py
@@ -7,6 +7,8 @@ class Booking(models.Model):
     last_name = models.CharField(max_length=200)
     guest_number = models.IntegerField()
     comment = models.CharField(max_length=1000)
+    reservation_date = models.DateField()
+    reservation_slot = models.SmallIntegerField(default=10)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
